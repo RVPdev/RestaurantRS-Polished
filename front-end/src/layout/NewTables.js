60cf9737cc156426f3197fe197a34c0fbeb6deed
RVPdev <renatovilcahuaman@gmail.com>
2023-10-20 18:23:47 -0500

    cleaning frontend

--- front-end/src/layout/NewTables.js
+++ front-end/src/layout/NewTables.js
@@ -3,35 +3,49 @@ import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 import { createTable } from "../utils/api";
 import ErrorAlert from "./ErrorAlert";
 
+// Function component for creating new tables
 function NewTables() {
+  // Initialize useHistory for route navigation
   const history = useHistory();
 
+  // Initial form state for table creation
   const initialFormState = {
     table_name: "",
     capacity: "",
   };
 
+  // Initialize state for form data
   const [formData, setFormData] = useState({ ...initialFormState });
 
+  // Initialize state for holding any table errors
   const [tablesError, setTablesError] = useState(null);
 
+  // Function to handle form input changes
   const handleChange = (event) => {
+    // Update form data state based on input changes
     setFormData({ ...formData, [event.target.name]: event.target.value });
   };
 
+  // Function to handle form submission
   const handleSubmit = async (event) => {
+    // Prevent default form submission behavior
     event.preventDefault();
 
-    const reservationFormated = {
+    // Format the form data before sending it
+    const reservationFormatted = {
       ...formData,
       capacity: Number(formData.capacity),
     };
 
-    await createTable(reservationFormated)
+    // API call to create a new table
+    await createTable(reservationFormatted)
+      // Navigate to the dashboard if successful
       .then(() => history.push(`/dashboard`))
+      // Set the error state if there's an API error
       .catch(setTablesError);
   };
 
+  // Render the form
   return (
     <div>
       <form className="row g-3 mt-2" onSubmit={handleSubmit}>
@@ -76,7 +90,7 @@ function NewTables() {
           Cancel
         </button>
       </form>
-      <ErrorAlert error={tablesError}/>
+      <ErrorAlert error={tablesError} />
     </div>
   );
 }
--- front-end/src/layout/ReservationSeat.js
+++ front-end/src/layout/ReservationSeat.js
@@ -1,22 +1,27 @@
 import React, { useEffect, useState } from "react";
 import { useHistory, useParams } from "react-router";
 import { listTables, readReservation, seatReservation } from "../utils/api";
-
 import ErrorAlert from "./ErrorAlert"
 
+// Function component for seating a reservation
 function ReservationSeat() {
+  // Initialize useHistory for route navigation and useParams to get reservation_id from URL
   const history = useHistory();
   const { reservation_id } = useParams();
 
+  // Initialize state for reservation data and errors
   const [reservation, setReservation] = useState({});
   const [reservationError, setReservationError] = useState(null);
 
+  // Initialize state for table errors and selected table ID
   const [tablesError, setTablesError] = useState(null);
   const [tableId, setTableId] = useState("");
 
+  // Initialize state for submit errors and available tables
   const [submitError, setSubmitError] = useState(null);
   const [availableTables, setAvailableTables] = useState([]);
 
+  // Fetch available tables
   useEffect(() => {
     const abortController = new AbortController();
     setTablesError(null);
@@ -28,6 +33,7 @@ function ReservationSeat() {
     return () => abortController.abort();
   }, []);
 
+  // Fetch reservation details
   useEffect(() => {
     const abortController = new AbortController();
     setReservationError(null);
@@ -38,69 +44,74 @@ function ReservationSeat() {
     return () => abortController.abort();
   }, [reservation_id]);
 
+  // Handler for changing selected table
   function changeHandler({ target: { value } }) {
     setTableId(value);
   }
 
+  // Handler for cancel button
   function onCancel() {
     history.goBack();
   }
 
+  // Handler for form submission
   function submitHandler(event) {
     event.preventDefault();
     event.stopPropagation();
     setSubmitError(null);
+
     seatReservation(reservation.reservation_id, tableId)
       .then(() => history.push("/dashboard"))
       .catch(setSubmitError);
   }
 
+  // Render form and other elements
   return (
     <main>
-    <h1>Seat Reservation</h1>
-    <ErrorAlert error={tablesError} />
-    <ErrorAlert error={reservationError} />
-    <ErrorAlert error={submitError} />
-    <h3>
-      # {reservation_id} - {reservation.first_name} {reservation.last_name} on{" "}
-      {reservation.reservation_date} at {reservation.reservation_time} for{" "}
-      {reservation.people}{" "}
-    </h3>
-    <form onSubmit={submitHandler}>
-      <fieldset>
-        <div className="row">
-          <div className="form-group col">
-            <label htmlFor="table_id">Seat at:</label>
-            <select
-              id="table_id"
-              name="table_id"
-              className="form-control"
-              value={tableId}
-              required={true}
-              onChange={changeHandler}
-            >
-              <option value="">Select a table</option>
-              {availableTables.map((table) => (
-                <option key={table.table_id} value={table.table_id}>
-                  {table.table_name} - {table.capacity}
-                </option>
-              ))}
-            </select>
+      <h1>Seat Reservation</h1>
+      <ErrorAlert error={tablesError} />
+      <ErrorAlert error={reservationError} />
+      <ErrorAlert error={submitError} />
+      <h3>
+        # {reservation_id} - {reservation.first_name} {reservation.last_name} on{" "}
+        {reservation.reservation_date} at {reservation.reservation_time} for{" "}
+        {reservation.people}{" "}
+      </h3>
+      <form onSubmit={submitHandler}>
+        <fieldset>
+          <div className="row">
+            <div className="form-group col">
+              <label htmlFor="table_id">Seat at:</label>
+              <select
+                id="table_id"
+                name="table_id"
+                className="form-control"
+                value={tableId}
+                required={true}
+                onChange={changeHandler}
+              >
+                <option value="">Select a table</option>
+                {availableTables.map((table) => (
+                  <option key={table.table_id} value={table.table_id}>
+                    {table.table_name} - {table.capacity}
+                  </option>
+                ))}
+              </select>
+            </div>
           </div>
-        </div>
-        <button
-          type="button"
-          className="btn btn-secondary mr-2 cancel"
-          onClick={onCancel}
-        >
-          <span className="oi oi-x" /> Cancel
-        </button>
-        <button type="submit" className="btn btn-primary">
-          <span className="oi oi-check" /> Submit
-        </button>
-      </fieldset>
-    </form>
-  </main>
+          <button
+            type="button"
+            className="btn btn-secondary mr-2 cancel"
+            onClick={onCancel}
+          >
+            <span className="oi oi-x" /> Cancel
+          </button>
+          <button type="submit" className="btn btn-primary">
+            <span className="oi oi-check" /> Submit
+          </button>
+        </fieldset>
+      </form>
+    </main>
   );
 }
 
